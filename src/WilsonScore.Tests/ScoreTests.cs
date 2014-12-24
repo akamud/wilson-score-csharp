174b08dea47ba95220788097b22590ae6973573a
Mahmoud Ali <muddibr@gmail.com>
2014-12-23 22:11:04 -0300

    Implementation and Tests project added

---
+++ src/WilsonScore.Tests/ScoreTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace WilsonScore.Tests
+{
+    public class ScoreTests
+    {
+        [Fact]
+        void WilsonScore_Should_Be_Zero_If_There_Are_No_Ups()
+        {
+            Assert.True(Wilson.Score(0, 2) == 0);
+        }
+
+        [Fact]
+        void WilsonScore_Should_Be_Lower_Than_Average_Score()
+        {
+            double expected = ((double)1 / (double)2);
+            Assert.True(Wilson.Score(1, 2) < expected);
+        }
+
+        [Fact]
+        void WilsonScore_Should_Be_Higher_For_Same_Average_But_Higher_Totals()
+        {
+            Assert.True(Wilson.Score(500, 1000) > Wilson.Score(1, 2));
+        }
+    }
+}
---
+++ src/WilsonScore/WilsonScore.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WilsonScore
+{
+    public static class Wilson
+    {
+        public static double Score(double up, double total, double confidence = 1.644853)
+        {
+            /** Based on http://www.evanmiller.org/how-not-to-sort-by-average-rating.html **/
+            if (total <= 0 || total < up)
+                return 0;
+
+            double phat = up / total;
+            double z2 = confidence * confidence;
+
+            return (phat + z2 / (2 * total) - confidence * Math.Sqrt((phat * (1 - phat) + z2 / (4 * total)) / total)) / (1 + z2 / total);
+        }
+    }
+}
