112330a50fb537ab831448b45ab34fcc1076117c
Mahmoud Ali <muddibr@gmail.com>
2015-11-16 19:36:56 -0300

    Add XML documentation

--- src/WilsonScore/Wilson.cs
+++ src/WilsonScore/Wilson.cs
@@ -6,6 +6,12 @@ namespace WilsonScore
 {
     public static class Wilson
     {
+        /// <summary>
+        /// Calculates the Wilson Score based on the total votes and upvotes
+        /// </summary>
+        /// <param name="up">Number of upvotes</param>
+        /// <param name="total">Total number of votes</param>
+        /// <param name="confidence">Confidence used in calculation, default 1.644853 (95%)</param>
         public static double Score(double up, double total, double confidence = 1.644853)
         {
             /** Based on http://www.evanmiller.org/how-not-to-sort-by-average-rating.html **/
@@ -18,6 +24,12 @@ namespace WilsonScore
             return (phat + z2 / (2 * total) - confidence * Math.Sqrt((phat * (1 - phat) + z2 / (4 * total)) / total)) / (1 + z2 / total);
         }
 
+        /// <summary>
+        /// Calculates the Wilson Score based on the total votes and upvotes
+        /// </summary>
+        /// <param name="up">Number of upvotes</param>
+        /// <param name="total">Total number of votes</param>
+        /// <param name="confidence">Confidence used in calculation, default 1.644853 (95%)</param>
         public static double Score(int up, int total, double confidence = 1.644853)
         {
             return Score((double) up, (double) total, confidence);
